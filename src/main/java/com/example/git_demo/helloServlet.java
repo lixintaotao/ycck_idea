88b692e01208b0d9b84a173fbf3c7574aab82c44
tt <3540465674@qq.com>
2023-06-13 14:24:31 +0800

    这是一个servlet类

--- src/main/java/com/example/git_demo/helloServlet.java
+++ src/main/java/com/example/git_demo/helloServlet.java
@@ -1,4 +1,21 @@
 package com.example.git_demo;
 
-public class helloServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/git")
+public class helloServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    super.doGet(req, resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    super.doPost(req, resp);
+  }
 }
